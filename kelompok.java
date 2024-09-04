c4d2d59957061797a6144c08767e1612f2bbee8a
HafizRizqi <hafizhernanda5@gmail.com>
2024-09-03 21:21:11 -0700

    Update kelompok.java

--- kelompok.java
+++ kelompok.java
@@ -22,5 +22,16 @@ public class kelompok {
         System.out.println("Alamat\t\t: Bogor");
         System.out.println("No HP\t\t: 082124494566");
         System.out.println("Hobi\t\t: Badminton");
+         System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : HAFIZ RIZQI HERNANDA");
+        System.out.println("NIM     : 244107020154");
+        System.out.println("Kelas   : TI_1B");
+        System.out.println("No Absen: 12");
+        System.out.println("Prodi   : Teknologi Informasi");
+        System.out.println("Alamat  : Jalan Senggani");
+        System.out.println("No HP   : 082143183969");
+        System.out.println("Hobi    : Membaca Komik");
     }
 }
