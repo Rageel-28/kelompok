d8764d1673b586b86a570527a648d729ff7f3304
Hikmal7cyber <mhikmal.ag@gmail.com>
2024-09-04 11:19:02 +0700

    Update kelompok.java

--- kelompok.java
+++ kelompok.java
@@ -11,5 +11,16 @@ public class kelompok {
         System.out.println("Alamat   : Jl Dieng Atas");
         System.out.println("No HP    : 0895630887958");
         System.out.println("Hobi     : Kuliner");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Muhammad Hikmal Al-Ghifary");
+        System.out.println("NIM\t\t: 244107020074");
+        System.out.println("Kelas\t\t: 1B");
+        System.out.println("No absen\t: 16");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: Bogor");
+        System.out.println("No HP\t\t: 082124494566");
+        System.out.println("Hobi\t\t: Badminton");
     }
 }
